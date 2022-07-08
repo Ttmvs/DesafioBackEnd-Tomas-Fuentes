4dac5eebc47f500503f6379a711c7122ac0b1a09
Tomás Fuentes <tomasfuentesm45@gmail.com>
2022-07-08 01:19:37 -0400

    se completó desafio

--- Contenedor.js
+++ Contenedor.js
@@ -4,12 +4,17 @@ class Contenedor {
     constructor(fileName){
         this.fileName = fileName;
     }
-    save (Object) {
+     save (Object) {
+        const data = this.getAll();
+        //console.log(`data: ${data}`)
+        let pets = JSON.parse(data);
+        //console.log(pets)
         const id = Math.floor(Math.random()*100);
         Object.id = id;
-        console.log(Object)
-        Object = JSON.stringify(Object, null, 2);
-        fs.writeFile(this.fileName, Object, (err)=>{
+        pets.mascotas.push(Object);
+        //console.log(Object)
+        pets = JSON.stringify(pets, null, 2);
+         fs.writeFile(this.fileName, pets, (err)=>{
             if(err){
                 console.log(err);
             }
@@ -18,19 +23,63 @@ class Contenedor {
     }
 
     getAll() {
-        const data = fs.readFileSync(this.fileName, 'utf8', (err) => {
-            
-            if (err) {
-                console.log(err);
-            }
-        })
+        const data = fs.readFileSync(this.fileName, 'utf8',);
         //data = JSON.parse(data);
-        console.log(`getAll: ${data}`)
+        //console.log(`getAll: ${data}`)
         return data;
         }
+
+    getById(id){
+        const data = this.getAll();
+        const pets = JSON.parse(data);
+        const {mascotas} = pets;
+        const pet = mascotas.find((x) => x.id==id);
+        if (pet){
+            return pet;
+        }else{
+            return null
+        }
+    }
+    deleteById(id){
+        const data = this.getAll();
+        const pets = JSON.parse(data);
+        const {mascotas} = pets;
+        const pet = mascotas.filter((x) => x.id!==id);
+        const x = {
+            mascotas: pet
+        };
+        fs.writeFile(this.fileName, JSON.stringify(x, null, 2), (err)=>{
+            if (err){
+                console.log(err);
+            }
+            
+        })
+        console.log(pet)
+    } 
+    deleteAll(){
+        const x = {
+            mascotas: []
+            
+        };
+        fs.writeFile(this.fileName, JSON.stringify(x, null, 2), (err)=>{
+            if (err){
+                console.log(err);
+            }
+            
+        })
+    }
+
 }
 
 const file = new Contenedor("Pets.json");
-const dogs = {id: 0, nombre: "Pepe", raza: "doberman"};
-//console.log(file.save(dogs))
-console.log(JSON.stringify(file.getAll()))
+const Pet = {id: 0, nombre:"aa", raza:"Doberman"};
+
+ /* file.save(Pet);
+setTimeout(function() {const listObject = JSON.stringify(file.getAll());
+    console.log(`listado: ${JSON.parse(listObject)}`)}, 1000); */
+ 
+ /* setTimeout(function() {const listObject = JSON.stringify(file.getById(8));
+        console.log(`listado a: ${listObject}`)}, 1500);  */
+ //console.log(file.getById(36)) 
+ //file.deleteById(8)
+ file.deleteAll()
